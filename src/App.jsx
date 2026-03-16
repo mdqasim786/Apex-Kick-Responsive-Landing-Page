a158b9f87eeb26f9a026d617fc99d74eb744ed03
white-devil5911 <muhammadqasim5911@gmail.com>
2026-03-16 12:56:13 +0500

    feat: Added tailwind css to my project

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
+      <h1 className="text-6xl font-bold underline text-center">
         Hello ApexKicks!
       </h1>
     </>
